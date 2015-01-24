f51edce9795c323622266dc978aeb45bd821abf8
Joshua James <joshua@cybercrimetech.com>
2015-01-24 18:40:47 +0900

    Update randomColor.sh

--- randomColor.sh
+++ randomColor.sh
@@ -9,18 +9,19 @@
 # Full path should be the full path to the script
 
 # Full path for cron job
-COLORDIR="/!!!FULL PATH!!!/msi-keyboard/examples"
-MODESDIR="/!!!FULL PATH!!!/msi-keyboard/exampleModes"
+BASEDIR="/home/!!Full path!!/msi-keyboard"
+COLORDIR="$BASEDIR/examples"
+MODESDIR="$BASEDIR/exampleModes"
 
 # Check of on battery. If yes, shut off keyboard
 # This could also be used to turn on only "low" lights
 if [ $(cat /sys/class/power_supply/ADP1/online) -eq 1 ]; then
 	# Get random color file
-	COLORFILE=`ls $COLORDIR | sort -R | tail -1`
+	COLORFILE=$(ls $COLORDIR | sort -R | tail -1)
 
-	echo "$COLORFILE selected"
-	node $COLORDIR/$COLORFILE
+	#echo "$COLORFILE selected"
+	$(/usr/local/bin/node $COLORDIR/$COLORFILE)
 else
-	echo "Laptop unplugged... switching off keyboard"
-	node $MODESDIR/off.js
+	#echo "Laptop unplugged... switching off keyboard"
+	$(/usr/local/bin/node $MODESDIR/off.js)
 fi
