3fbe660eabe6a8ae294f923a09e8d96cecc9083d
Steve Lacy <me@slacy.me>
2015-04-08 22:46:54 -0700

    update help info with colors + add error check

--- examples/msi-kbd.js
+++ examples/msi-kbd.js
@@ -1,54 +1,59 @@
-// msi-kbd - Change color settings of MSI laptop keyboards 
+// msi-kbd - Change color settings of MSI laptop keyboards
 // (try 'msi-kbd --help' for options and settings or see Usage())
 //
 // Hal Pomeranz (hal@deer-run.com), Deer Run Associates, 2015-04-08
 // This code released under Creative Commons Attribution license (CC BY)
 
-var colors = ['off', 
-	      'red', 
-	      'orange', 
-	      'yellow', 
-	      'green', 
-              'sky', 
-	      'blue', 
-	      'purple', 
-	      'white'];
+var keyboard = require('msi-keyboard');
+var argv = require('minimist')(process.argv.slice(2), {
+  string: 'clmrM'.split(''),
+  unknown: Usage
+});
 
+var colors = [
+  'off',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'sky',
+  'blue',
+  'purple',
+  'white'
+];
 var intensities = ['light', 'low', 'med', 'high'];
-
 var modes = ['normal', 'gaming', 'breathe', 'demo', 'wave'];
 
-var argv = require('minimist')(process.argv.slice(2), 
-			       {string: 'ClmrM'.split(''), unknown: Usage});
-var keyboard = require('msi-keyboard');
-
-if (argv['C']) {
-  LightKbd('left', argv['C']);
-  LightKbd('middle', argv['C']);
-  LightKbd('right', argv['C']);
+if (Object.keys(argv).length < 2) {
+  return Usage();
 }
 
-if (argv['l']) {
-  LightKbd('left', argv['l']);
+if (argv.c) {
+  LightKbd('left', argv.c);
+  LightKbd('middle', argv.c);
+  LightKbd('right', argv.c);
 }
 
-if (argv['m']) {
-  LightKbd('middle', argv['m']);
+if (argv.l) {
+  LightKbd('left', argv.l);
 }
 
-if (argv['r']) {
-  LightKbd('right', argv['r']);
+if (argv.m) {
+  LightKbd('middle', argv.m);
 }
 
-if (argv['M']) {
-  if (modes.indexOf(argv['M'].toLowerCase()) > -1) {
-    keyboard.mode(argv['M']);
+if (argv.r) {
+  LightKbd('right', argv.r);
+}
+
+if (argv.M) {
+  if (modes.indexOf(argv.M.toLowerCase()) > -1) {
+    keyboard.mode(argv.M);
   }
   else {
-    Usage('Unrecognized mode ' + argv['M']);
+    Usage('Unrecognized mode ' + argv.M);
   }
 }
-
 
 function LightKbd(region, colorstr) {
   colorstr = colorstr.toLowerCase();
@@ -73,7 +78,7 @@ function LightKbd(region, colorstr) {
   }
 
   if (!colorSet.length) {
-    Usage('You must specify a color for ' + region + ' region');
+    return Usage('You must specify a color for ' + region + ' region');
   }
 
   keyboard.color(region, {color:colorSet, intensity:intenSet});
@@ -81,13 +86,14 @@ function LightKbd(region, colorstr) {
 
 
 function Usage(msg) {
-  if (msg.indexOf('-') !== 0) {
+  if (msg) {
     process.stderr.write(msg + '\n');
   }
   process.stderr.write('Usage: msi-kbd [-C colorspec | -l colorspec -m colorspec -r colorspec] [-M mode]\n');
-  process.stderr.write('   Colorspec should be color[:intensity]\n');
-  process.stderr.write('      Valid colors: black,none,' + colors + '\n');
-  process.stderr.write('      Valid intensities: ' + intensities + '\n');
-  process.stderr.write('   Valid modes: ' + modes + '\n');
+  process.stderr.write('   Colorspec should be color:intensity\n');
+  process.stderr.write('      Valid colors: ' + colors.join(', ') + '\n');
+  process.stderr.write('      Valid intensities: ' + intensities.join(', ') + '\n');
+  process.stderr.write('   Valid modes: ' + modes.join(', ') + '\n');
+  process.stderr.write('sudo node msi-kbd.js -C blue -l green -r red:high' + '\n');
   process.exit(1);
 }
