b11a51ab367f7ad3d15bbe07f27e235a5f4fa396
Corin Lawson <corin@phiware.com.au>
2015-04-27 19:52:20 +1000

    Reorder tests in order to follow preferred coding style

--- test/main.js
+++ test/main.js
@@ -1,9 +1,9 @@
+var keyboard = require('../');
 var should = require('should');
 require('mocha');
 
 describe('msi-keyboard', function() {
   it('should export a keyboard', function(done) {
-    var keyboard = require('../');
     should.exist(keyboard);
     keyboard.close();
     done();
