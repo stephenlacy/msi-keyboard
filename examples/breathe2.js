f05a3dbcddffd081a39470867ad1b3e7cbcef432
Jérémy Munsch <jeremy.munsch@gmail.com>
2015-12-01 12:39:49 +0100

    fix merge regression on previous commit concerning modes regions adresses are now properly assigned

---
+++ examples/breathe2.js
@@ -0,0 +1,7 @@
+var keyboard = require('../');
+
+keyboard.mode('breathe', {
+        left: {color:'blue',secondary:'red'},
+        midle: {color:'blue', secondary:'red'},
+        right: {color:'blue', secondary:'red'},
+    });
