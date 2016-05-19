0dc47701390009a856dea1a5ba15477525cda6f9
Steve Lacy <me@slacy.me>
2016-05-18 19:18:58 -0700

    fix example variable

--- examples/breathe2.js
+++ examples/breathe2.js
@@ -2,6 +2,6 @@ var keyboard = require('../')();
 
 keyboard.mode('breathe', {
   left: {color:'blue',secondary:'red'},
-  midle: {color:'blue', secondary:'red'},
+  middle: {color:'blue', secondary:'red'},
   right: {color:'blue', secondary:'red'},
 });
